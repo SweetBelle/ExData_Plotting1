4a958042e59d030182ff3d0fab7c884b27eae1c9
Jobelle Simblante <37974270+SweetBelle@users.noreply.github.com>
2018-07-20 21:38:15 +0800

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+subsetdata <- subset(data, Date == "2007-02-01" | Date =="2007-02-02")
+png("plot1.png", width=480, height=480)
+hist(as.numeric(as.character(subsetdata$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+dev.off()
